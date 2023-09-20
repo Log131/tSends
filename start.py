bcc4ae76f46faa308d0e37d10d0855631c8fe8f0
Log131 <87196980+Log131@users.noreply.github.com>
2023-09-20 15:59:21 +0300

    Add files via upload

--- start.py
+++ start.py
@@ -34,8 +34,11 @@ ListenPort = 51830
 PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 
+
+[Peer]
 PublicKey = {s}
-AllowedIPs = 10.0.0.2/32
+AllowedIPs = 10.0.0.2/3
+
 
 """
 
@@ -53,4 +56,25 @@ subprocess.run(['sudo', 'systemctl', 'start', 'wg-quick@wg5.service'])
 subprocess.run(['sudo', 'systemctl', 'status', 'wg-quick@wg5.service'])
 
 
+with open('/root/publickey', 'r') as files:
+    s_ = files.read()
 
+
+
+
+with open('/root/private_key', 'r') as files_:
+    s_5 = files.read()
+
+pub_config = f""""[Interface]
+PrivateKey = {s_5}
+Address = 10.0.0.2/32
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = {s_}
+Endpoint = 45.129.3.8:51830
+AllowedIPs = 0.0.0.0/0
+"""
+
+with open('/root/tsends/qwe.conf', 'w') as files_5:
+    files_5.write(pub_config)
