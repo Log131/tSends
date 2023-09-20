47c5281891c76fe8340c713818a957942bcfd1d6
Log131 <87196980+Log131@users.noreply.github.com>
2023-09-20 15:31:09 +0300

    Add files via upload

--- start.py
+++ start.py
@@ -2,7 +2,6 @@ import subprocess
 
 
 
-subprocess.run('wg', 'genkey', '|', 'tee', 'privatekey', '|', 'wg', 'pubkey' '>', 'publickey')
 
 
 
@@ -12,18 +11,32 @@ subprocess.run('wg', 'genkey', '|', 'tee', 'privatekey', '|', 'wg', 'pubkey' '>'
 with open('/root/privatekey', 'r')as f:
     private_key = f.read().strip()
 
-with open('/root/publickey', 'r') as files:
-    public_key = files.read().strip()
+
+with open('root/pub_key', 'r')as f_:
+    s = f_.read()
+
+
+
+
+
+
+
 
 server_config = f"""[Interface]
 PrivateKey = {private_key}
-Address = 10.0.0.2/32
-DNS = 8.8.8.8
+Address = 10.0.0.1/32
+ListenPort = 51830
 
-[Peer]
-PublicKey = {public_key}
-Endpoint = 45.129.3.8:51830
-AllowedIPs = 0.0.0.0/0
+
+
+
+
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+
+PublicKey = {s}
+AllowedIPs = 10.0.0.2/32
+
 """
 
 
@@ -32,9 +45,12 @@ with open('/etc/wireguard/wg5.conf', 'w') as files_:
     files_.write(server_config)
 
 
-subprocess.run(['sudo', 'systemctl', 'enable', 'wg-quick@wg5'])
-subprocess.run(['sudo', 'systemctl', 'start', 'wg-quick@wg5'])
+subprocess.run(['sudo', 'systemctl', 'enable', 'wg-quick@wg5.service'])
+subprocess.run(['sudo', 'systemctl', 'start', 'wg-quick@wg5.service'])
 
 
 
-subprocess.run(['sudo', 'systemctl', 'status', 'wg-quick@wg5'])
+subprocess.run(['sudo', 'systemctl', 'status', 'wg-quick@wg5.service'])
+
+
+
