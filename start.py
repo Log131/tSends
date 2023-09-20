5b3e7066de09058bd1e389b8fc8deab74fac092a
Log131 <87196980+Log131@users.noreply.github.com>
2023-09-20 14:53:41 +0300

    Add files via upload

--- start.py
+++ start.py
@@ -2,31 +2,18 @@ import subprocess
 
 
 
-subprocess.run(['sudo', 'apt', 'update'])
-subprocess.run(['sudo', 'apt', 'upgrade', '-y'])
-subprocess.run(['sudo', 'apt', 'install', '-y', 'wireguard'])
-subprocess.run(['sudo', 'umask', '077'])
-subprocess.run(['sudo', 'wg', 'genkey', '|', 'tee', '/etc/wireguard/privatekey'])
-subprocess.run(['sudo', 'wg', 'pubkey', '|', 'tee', '/etc/wireguard/privatekey', '|', 'wg', 'pubkey', '|', 'tee', 'etc/wireguard/publickey'])
-
-
-subprocess.run(['echo', 'net.ipv4.ip_forward=1', '>>', '/etc/sysctl.conf'])
-
-subprocess.run(['sysctl', '-p'])
-subprocess.run(['sudo', 'wg', 'genkey', '|', 'tee', '/etc/wireguard/qwe', '|', 'wg', 'pubkey', '|', 'tee', '/etc/wireguard/qwe'])
+subprocess.run('wg', 'genkey', '|', 'tee', 'privatekey', '|', 'wg', 'pubkey' '>', 'publickey')
 
 
 
 
 
 
+with open('/root/privatekey', 'r')as f:
+    private_key = f.read().strip()
 
-
-with open('/etc/wireguard/qwe', 'r')as f:
-    pub_key = f.read().strip()
-
-with open('/etc/wireguard/publickey', 'r') as files:
-    private_key = files.read().strip()
+with open('/root/publickey', 'r') as files:
+    public_key = files.read().strip()
 
 server_config = f"""[Interface]
 PrivateKey = {private_key}
@@ -34,15 +21,14 @@ Address = 10.0.0.2/32
 DNS = 8.8.8.8
 
 [Peer]
-PublicKey = {pub_key}
-Endpoint = server:51830
+PublicKey = {public_key}
+Endpoint = 45.129.3.8:51830
 AllowedIPs = 0.0.0.0/0
-PersistentKeepalive = 20
 """
 
 
 
-with open('/etc/wireguard/w5.conf', 'w') as files_:
+with open('/etc/wireguard/wg5.conf', 'w') as files_:
     files_.write(server_config)
 
 
